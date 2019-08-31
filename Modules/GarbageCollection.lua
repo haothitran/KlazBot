a0360d0ba9c81b9b044e18e8e0b8a9ca570a9e84
Hao Tran <hello@haothitran.com>
2019-08-31 09:59:47 -0400

    Fix code syntax

--- Modules/GarbageCollection.lua
+++ Modules/GarbageCollection.lua
@@ -9,7 +9,8 @@ garbage:RegisterAllEvents()
 garbage:SetScript('OnEvent', function(self, event)
   eventcount = eventcount + 1
 
-  if event == 'PLAYER_ENTERING_WORLD' or (InCombatLockdown() and eventcount > 25000)
+  if event == 'PLAYER_ENTERING_WORLD'
+  or (InCombatLockdown() and eventcount > 25000)
   or (not InCombatLockdown() and eventcount > 10000) then
     collectgarbage()
     eventcount = 0
